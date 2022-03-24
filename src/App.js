66c802185c89e6a8d25bf64970fe3bbda96420c2
MMaidul <maidulmuqtadir360@gmail.com>
2022-03-24 11:55:31 +0600

    api data loaded and display in ui done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import MealDB from './componets/MealDB/MealDB';
 
 function App() {
+
+  const [foods, setFoods] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=a`)
+      .then(res => res.json())
+      .then(data => setFoods(data.meals))
+  }, []);
   return (
     <div className='App'>
-      <MealDB />
+      <div className="food-container">
+        {
+          foods.map(food => <MealDB
+            food={food}
+          ></MealDB>)
+        }
+      </div>
     </div>
   );
 }
--- src/componets/MealDB/MealDB.js
+++ src/componets/MealDB/MealDB.js
@@ -1,23 +1,22 @@
-import React from 'react';
 import './MealDB.css';
 
-const MealDB = () => {
+const MealDB = ({ food }) => {
+    const { strMealThumb, strMeal, strInstructions } = food;
     return (
-        <div className='food-container'>
-
-            <img src="https://www.themealdb.com/images/media/meals/58oia61564916529.jpg" alt="" />
-            <h2>Corba</h2>
+        <div className='food-containerer'>
+            <img src={strMealThumb} alt="" />
+            <h2>Name: {strMeal}</h2>
 
             <div className="food-hover">
                 <div className="food-hover__img">
-                    <img src="https://www.themealdb.com/images/media/meals/58oia61564916529.jpg" alt="" />
+                    <img src={strMealThumb} alt="" />
                 </div>
                 <div className="food-hover__info">
-                    <p>Pick through your lentils for any foreign debris, rinse them 2 or 3 times, drain, and set aside.  Fair warning, this will probably turn your lentils into a solid block that you’ll have to break up later\r\nIn a large pot over medium-high heat, sauté the olive oil.</p>
-                    <button>Learn from Youtube</button>
+                    <h4>Name: {strMeal}</h4>
+                    <p>{strInstructions.slice(0, 250)}</p>
+                    <a href="">Learn from Youtube</a>
                 </div>
             </div>
-
         </div>
     );
 };
